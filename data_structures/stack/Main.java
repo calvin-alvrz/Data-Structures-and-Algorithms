0877a736356f64a28c33dcb78d677bbb05d017dc
Calvin Alvarez <calvin_alvarez@outlook.com>
2023-12-10 08:47:02 +1300

    v0.1.0

--- data_structures/stack/Main.java
+++ data_structures/stack/Main.java
@@ -3,17 +3,32 @@ package data_structures.stack;
 public class Main {
     public static void main(String[] args) {
         
-        ArrayStack stack = new ArrayStack(10);
+        // ArrayStack stack = new ArrayStack(10); // For Arrays
         
-        stack.push(new Employee("Jane", "Jones", 123));
-        stack.push(new Employee("John", "Doe", 123));
-        stack.push(new Employee("Mary", "Smith", 123));
-        stack.push(new Employee("Mike", "Wilson", 123));
-        stack.push(new Employee("Bill", "End", 123));
+        /*stack.push(new Employee("Jane", "Jones", 123));
+        stack.push(new Employee("John", "Doe", 4567));
+        stack.push(new Employee("Mary", "Smith", 22));
+        stack.push(new Employee("Mike", "Wilson", 3245));
+        stack.push(new Employee("Bill", "End", 78));*/
+
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billEnd = new Employee("Bill", "End", 78);
+
+        LinkedStack stack = new LinkedStack(); // For Linked List
+
+        stack.push(janeJones);
+        stack.push(johnDoe);
+        stack.push(marySmith);
+        stack.push(mikeWilson);
+        stack.push(billEnd);
 
         //stack.printStack();
 
-        System.out.println(stack.peek());
+        //System.out.println(stack.peek());
+        //stack.printStack();
 
         System.out.println("Popped: " + stack.pop());
         System.out.println(stack.peek());
