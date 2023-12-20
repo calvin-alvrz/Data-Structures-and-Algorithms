3f0a467faa4aad27f8474c1fef6239ef5cbca5a6
Calvin Alvarez <calvin_alvarez@outlook.com>
2023-12-20 22:44:33 +1300

    v0.2.0

--- data_structures/queue/ArrayQueue.java
+++ data_structures/queue/ArrayQueue.java
@@ -14,9 +14,15 @@ public class ArrayQueue {
 
     public void add(Employee employee) {
         if (size() == queue.length - 1) {
+            int numItems = size();
             Employee[] newArray = new Employee[2 * queue.length];
-            System.arraycopy(queue, 0, newArray, 0, queue.length);
+            
+            System.arraycopy(queue, front, newArray, 0, queue.length - front);
+            System.arraycopy(queue, 0, newArray, queue.length - front, back);
+            
             queue = newArray;
+            front = 0;
+            back = numItems;
         }
 
         queue[back] = employee;
@@ -40,6 +46,9 @@ public class ArrayQueue {
             front = 0;
             back = 0;
         }
+        else if (front == queue.length) {
+            front = 0;
+        }
 
         return employee;
     }
@@ -53,12 +62,24 @@ public class ArrayQueue {
     }
 
     public int size() {
-        return back - front;
+        if (front <= back) {
+            return back - front;
+        }
+        else {
+            return back - front + queue.length;
+        }
     }
 
     public void printQueue() {
-        for (int i = front; i < back; i++) {
-            System.out.println(queue[i]);
+        if (front <= back) {
+            for (int i = front; i < back; i++) {
+                System.out.println(queue[i]);
+            }
+        }
+        else {
+            for (int i = front; i < queue.length; i++) {
+                System.out.println(queue[i]);
+            }
         }
     }
 }
