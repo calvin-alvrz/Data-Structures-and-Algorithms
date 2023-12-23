2da3dd693bb525c065ca88cdbdc0120b60ea5b1c
Calvin Alvarez <calvin_alvarez@outlook.com>
2023-12-23 22:17:48 +1300

    v0.3.0

---
+++ data_structures/hashtables/StoredEmployee.java
@@ -0,0 +1,12 @@
+package data_structures.hashtables;
+
+public class StoredEmployee {
+    
+    public String key;
+    public Employee employee;
+
+    public StoredEmployee(String key, Employee employee) {
+        this.key = key;
+        this.employee = employee;
+    }
+}
