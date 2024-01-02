b6fd7671b6ad1b41cb35488de88c82cbc8c8fd1f
Calvin Alvarez <calvin_alvarez@outlook.com>
2024-01-03 12:25:21 +1300

    v0.3.2

--- data_structures/hashtables/Main.java
+++ data_structures/hashtables/Main.java
@@ -7,7 +7,6 @@ public class Main {
         Employee johnDoe = new Employee("John", "Doe", 4567);
         Employee marySmith = new Employee("Mary", "Smith", 22);
         Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
-        Employee billEnd = new Employee("Bill", "End", 78);
 
         SimpleHashtable ht = new SimpleHashtable();
         ht.put("Jones", janeJones);
