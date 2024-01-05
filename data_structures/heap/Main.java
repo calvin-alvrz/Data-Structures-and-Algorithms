5e8d09f2f446806d6cc496a2fea74feea944dab0
Calvin Alvarez <calvin_alvarez@outlook.com>
2024-01-05 19:49:16 +1300

    v0.4.0

--- data_structures/heap/Main.java
+++ data_structures/heap/Main.java
@@ -1,5 +1,21 @@
 package data_structures.heap;
 
 public class Main {
-    
+   public static void main(String[] args) {
+        MaxHeap heap = new MaxHeap(10);
+
+        heap.insert(80);
+        heap.insert(75);
+        heap.insert(60);
+        heap.insert(68);
+        heap.insert(55);
+        heap.insert(40);
+        heap.insert(52);
+        heap.insert(67);
+
+        heap.printHeap();
+
+        heap.delete(1);
+        heap.printHeap();
+   }
 }
